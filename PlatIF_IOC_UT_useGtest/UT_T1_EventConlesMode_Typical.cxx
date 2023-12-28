687981c5b6314922d8061fa5399c9e8fcca950e0
EnigmaWu <286476019@qq.com>
2023-12-28 22:12:32 +0800

    [WIP]: PlatIF_IOC

--- PlatIF_IOC_UT_useGtest/UT_T1_EventConlesMode_Typical.cxx
+++ PlatIF_IOC_UT_useGtest/UT_T1_EventConlesMode_Typical.cxx
@@ -2,16 +2,14 @@
 
 // This is IOC_Event in ConlesMode's typical UT from API user's perspective
 //===>Case[01]: Ref::ConlesMode's call flow typical example[1] in PlatIF_IOC.h
-/**
- * Step1: EvtSuber as ObjX subEvt(EvtID==IOC_EVTID_TEST_KEEPALIVE)
- * Step2: EvtPuber as ObjY postEVT(EvtID==IOC_EVTID_TEST_KEEPALIVE*$_UT_EVTCNT_KEEPALIVE)
- * Step3: EvtSuber's EvtArgs::CbProcEvt_F is called $$_UT_EVTCNT_KEEPALIVE times 
- * Step4: EvtSuber unsubEvt(EvtID==IOC_EVTID_TEST_KEEPALIVE) 
- * Step5: EvtPuber postEVT(EvtID==IOC_EVTID_TEST_KEEPALIVE*$_UT_EVTCNT_KEEPALIVE) again 
- * Step6: EvtSuber's EvtArgs::CbProcEvt_F is NOT called
- */
-//===>Case[02]: ...
+//-Step1: EvtSuber as ObjX subEvt(EvtID==IOC_EVTID_TEST_KEEPALIVE)
+//-Step2: EvtPuber as ObjY postEVT(EvtID==IOC_EVTID_TEST_KEEPALIVE*$_UT_EVTCNT_KEEPALIVE)
+//-Step3: EvtSuber's EvtArgs::CbProcEvt_F is called $$_UT_EVTCNT_KEEPALIVE times
+//-Step4: EvtSuber unsubEvt(EvtID==IOC_EVTID_TEST_KEEPALIVE)
+//-Step5: EvtPuber postEVT(EvtID==IOC_EVTID_TEST_KEEPALIVE*$_UT_EVTCNT_KEEPALIVE) again
+//-Step6:EvtSuber's EvtArgs::CbProcEvt_F is NOT called
 
+//===>Case[02]: ...
 #define _UT_EVTCNT_KEEPALIVE 1024
 
 TEST(EventConlesModeTypical, Case01) {
