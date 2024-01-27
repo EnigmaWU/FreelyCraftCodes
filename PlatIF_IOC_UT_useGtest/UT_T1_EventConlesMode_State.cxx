7c003894d6f86f06a331ce8762aa787c2c2de9cb
EnigmaWU <enigma0702@gmail.com>
2024-01-27 16:02:54 +0800

    Add test case for ObjB postEvt in a thread context

--- PlatIF_IOC_UT_useGtest/UT_T1_EventConlesMode_State.cxx
+++ PlatIF_IOC_UT_useGtest/UT_T1_EventConlesMode_State.cxx
@@ -56,3 +56,79 @@ TEST(ConlesModeState, Case01) {
   // Check ObjA's private magic value is Y now
   ASSERT_EQ(CbPrivObjA.MagicValue, 0x87654321);  // CheckPoint
 }
+
+//===> Case[02]: Same as Case[01], but ObjB postEvt in a thread context
+//  Step-1: Same with Case[01]-Step-1
+//  Step-2: Start Thread_ObjB to postEvt(TEST_KEEPALIVE)
+//  Step-3: ObjA wait its CbProcEvt_F is callbacking as its private magic value is Y now
+//  Step-4: ObjA unsubEvt(TEST_KEEPALIVE)
+//  Step-5: Check ObjA's private magic value is Z now
+
+typedef enum {
+  _UT_Case02_MagicValue_X = 0x12345678,
+  _UT_Case02_MagicValue_Y = 0x87654321,
+  _UT_Case02_MagicValue_Z = 0x13572468,
+} _UT_Case02_MagicValue_T;
+
+typedef struct {
+  _UT_Case02_MagicValue_T MagicValue;
+} _UT_Case02_CbPrivObjA_T, *_UT_Case02_CbPrivObjA_pT;
+
+static TOS_Result_T _UT_Case02_CbProcEvtObjA_F(IOC_EvtDesc_pT pEvtDesc, void* pCbPriv) {
+  _UT_Case02_CbPrivObjA_pT pCbPrivObjA = (_UT_Case02_CbPrivObjA_pT)pCbPriv;
+  pCbPrivObjA->MagicValue = _UT_Case02_MagicValue_Y;
+  sleep(3);
+  return TOS_RESULT_SUCCESS;
+}
+
+static void* _UT_Case02_ThreadObjB(void* pArg) {
+  IOC_LinkID_T LinkID = IOC_CONLESMODE_AUTO_LINK_ID;
+  IOC_EvtDesc_T EvtDescObjB = {.EvtID = IOC_EVTID_TEST_KEEPALIVE};
+
+  while (1) {
+    PLT_IOC_postEVT(LinkID, &EvtDescObjB, NULL);
+    usleep(1);
+  }
+
+  return NULL;
+}
+
+TEST(ConlesModeState, Case02) {
+  IOC_LinkID_T LinkID = IOC_CONLESMODE_AUTO_LINK_ID;
+
+  // Step-1: ObjA as EvtSuber subEvt(TEST_KEEPALIVE), set ObjA's private magic value X
+  _UT_Case02_CbPrivObjA_T CbPrivObjA = {};
+  CbPrivObjA.MagicValue = _UT_Case02_MagicValue_X;
+
+  IOC_EvtID_T EvtIDsObjA[] = {IOC_EVTID_TEST_KEEPALIVE};
+  IOC_EvtSubArgs_T EvtSubArgsObjA = {
+      .CbProcEvt_F = _UT_Case02_CbProcEvtObjA_F,
+      .pCbPriv = &CbPrivObjA,
+      .EvtNum = TOS_calcArrayElmtCnt(EvtIDsObjA),
+      .pEvtIDs = EvtIDsObjA,
+  };
+  TOS_Result_T Result = PLT_IOC_subEVT(LinkID, &EvtSubArgsObjA);
+  ASSERT_EQ(Result, TOS_RESULT_SUCCESS);  // CheckPoint
+
+  // Step-2: Start Thread_ObjB to postEvt(TEST_KEEPALIVE)
+  pthread_t Thread_ObjB;
+  int PSXRet = pthread_create(&Thread_ObjB, NULL, _UT_Case02_ThreadObjB, NULL);
+  ASSERT_EQ(PSXRet, 0);  // CheckPoint
+
+  // Step-3: ObjA wait its CbProcEvt_F is callbacking
+  while (CbPrivObjA.MagicValue != _UT_Case02_MagicValue_Y) {
+    usleep(1);
+  }
+
+  // Step-4: ObjA unsubEvt(TEST_KEEPALIVE)
+  IOC_EvtUnsubArgs_T EvtUnsubArgsObjA = {.CbProcEvt_F = _UT_Case01_CbProcEvtObjA_F, .pCbPriv = &CbPrivObjA};
+  Result = PLT_IOC_unsubEVT(LinkID, &EvtUnsubArgsObjA);
+  ASSERT_EQ(Result, TOS_RESULT_SUCCESS);  // CheckPoint
+
+  // Step-5: Check ObjA's private magic value is Y now
+  ASSERT_EQ(CbPrivObjA.MagicValue, _UT_Case02_MagicValue_Z);  // CheckPoint
+
+  // TEARDOWN: Cancel&Wait Thread_ObjB exit
+  pthread_cancel(Thread_ObjB);
+  pthread_join(Thread_ObjB, NULL);
+}
