1069dd51075874c7cd65b5d55acb3f37c843ea38
EnigmaWU <enigma0702@gmail.com>
2024-02-07 16:52:11 +0800

    Refactor event posting in concurrency test

--- PlatIF_IOC_UT_useGtest/UT_T1_EventConlesMode_Concurrency.cxx
+++ PlatIF_IOC_UT_useGtest/UT_T1_EventConlesMode_Concurrency.cxx
@@ -42,20 +42,18 @@ static TOS_Result_T _UT_Case01_CbProcEvtObjA_F(IOC_EvtDesc_pT pEvtDesc, void* pC
 #define _UT_EVT_CNT_NONBLOCK 1000000
 
 static void* _UT_Case01_ThreadObjB(void* pArg) {
-  IOC_LinkID_T LinkID = IOC_CONLESMODE_AUTO_LINK_ID;
   IOC_EvtDesc_T EvtDescObjB = {.EvtID = IOC_EVTID_TEST_BLOCK_SLEEP_1MS};
 
   for (int i = 0; i < _UT_EVT_CNT_BLOCK; i++) {
     struct timeval BeforePostTime, AfterPostTime;
 
     gettimeofday(&BeforePostTime, NULL);
-    TOS_Result_T Result = PLT_IOC_postEVT(LinkID, &EvtDescObjB, NULL);
+    TOS_Result_T Result = PLT_IOC_postEVT_inConlesMode(&EvtDescObjB, NULL);
     gettimeofday(&AfterPostTime, NULL);
 
     EXPECT_EQ(Result, TOS_RESULT_SUCCESS);  // CheckPoint
 
-    unsigned long PostTimeUS =
-        (AfterPostTime.tv_sec - BeforePostTime.tv_sec) * 1000000 + AfterPostTime.tv_usec - BeforePostTime.tv_usec;
+    ULONG_T PostTimeUS = (AfterPostTime.tv_sec - BeforePostTime.tv_sec) * 1000000 + AfterPostTime.tv_usec - BeforePostTime.tv_usec;
     EXPECT_LE(PostTimeUS, 8000);  // CheckPoint@Performance_FAST(<=100us)@MAYBLOCK(<=5ms + ~3ms)
 
     usleep(30);
@@ -69,7 +67,6 @@ typedef struct {
 
 static void* _UT_Case01_ThreadObjC(void* pArg) {
   _UT_Case01_CbPrivObjC_pT pCbPrivObjC = (_UT_Case01_CbPrivObjC_pT)pArg;
-  IOC_LinkID_T LinkID = IOC_CONLESMODE_AUTO_LINK_ID;
   IOC_EvtDesc_T EvtDescObjC = {.EvtID = IOC_EVTID_TEST_NONBLOCK_SLEEP_5MS};
 
   for (int i = 0; i < _UT_EVT_CNT_NONBLOCK; i++) {
@@ -77,7 +74,7 @@ static void* _UT_Case01_ThreadObjC(void* pArg) {
     struct timeval BeforePostTime, AfterPostTime;
 
     gettimeofday(&BeforePostTime, NULL);
-    TOS_Result_T Result = PLT_IOC_postEVT(LinkID, &EvtDescObjC, &OptTimeout);
+    TOS_Result_T Result = PLT_IOC_postEVT_inConlesMode(&EvtDescObjC, &OptTimeout);
     gettimeofday(&AfterPostTime, NULL);
 
     if (Result == TOS_RESULT_TIMEOUT) {
@@ -86,8 +83,7 @@ static void* _UT_Case01_ThreadObjC(void* pArg) {
       EXPECT_EQ(Result, TOS_RESULT_SUCCESS);  // CheckPoint
     }
 
-    unsigned long PostTimeUS =
-        (AfterPostTime.tv_sec - BeforePostTime.tv_sec) * 1000000 + AfterPostTime.tv_usec - BeforePostTime.tv_usec;
+    ULONG_T PostTimeUS = (AfterPostTime.tv_sec - BeforePostTime.tv_sec) * 1000000 + AfterPostTime.tv_usec - BeforePostTime.tv_usec;
     EXPECT_LE(PostTimeUS, 100);  // CheckPoint@Performance_FAST(<=100us)@NONBLOCK
 
     usleep(1);
@@ -96,8 +92,6 @@ static void* _UT_Case01_ThreadObjC(void* pArg) {
 }
 
 TEST(UT_ConlesModeEventConcurrency, Case01) {
-  IOC_LinkID_T LinkID = IOC_CONLESMODE_AUTO_LINK_ID;
-
   //===SETUP===
   // Step-1: ObjA as EvtSuber subEvt(TEST_BLOCK_SLEEP_5MS, TEST_NONBLOCK_SLEEP_1MS)
   _UT_Case01_CbPrivObjA_T CbPrivObjA = {.EvtCntBlockSleep1MS = 0, .EvtCntNonBlockSleep5MS = 0};
@@ -109,7 +103,7 @@ TEST(UT_ConlesModeEventConcurrency, Case01) {
       .pEvtIDs = EvtIDsObjA,
   };
 
-  TOS_Result_T Result = PLT_IOC_subEVT(LinkID, &EvtSubArgsObjA);
+  TOS_Result_T Result = PLT_IOC_subEVT_inConlesMode(&EvtSubArgsObjA);
   ASSERT_EQ(Result, TOS_RESULT_SUCCESS);  // CheckPoint
 
   //===EXECUTE===
@@ -140,6 +134,6 @@ TEST(UT_ConlesModeEventConcurrency, Case01) {
       .CbProcEvt_F = _UT_Case01_CbProcEvtObjA_F,
       .pCbPriv = &CbPrivObjA,
   };
-  Result = PLT_IOC_unsubEVT(LinkID, &EvtUnsubArgsObjA);
+  Result = PLT_IOC_unsubEVT_inConlesMode(&EvtUnsubArgsObjA);
   ASSERT_EQ(Result, TOS_RESULT_SUCCESS);  // Check
 }
