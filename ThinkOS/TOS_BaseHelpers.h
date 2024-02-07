48054b94649bed1f9de24bf4f5aadba464c17aef
EnigmaWU <enigma0702@gmail.com>
2024-02-07 12:20:13 +0800

    Add TOS_abortNotTested macro to TOS_BaseHelpers.h

--- ThinkOS/TOS_BaseHelpers.h
+++ ThinkOS/TOS_BaseHelpers.h
@@ -3,4 +3,10 @@
 
 #define TOS_calcArrayElmtCnt(array) (sizeof(array)/sizeof(array[0]))
 
+#define TOS_abortNotTested()                                     \
+  do {                                                           \
+    printf("Not tested %s@%s:%d\n"__func__, __FILE__, __LINE__); \
+    abort();                                                     \
+  } while (0)
+
 #endif//__TOS_BASE_HELPERS_H__
