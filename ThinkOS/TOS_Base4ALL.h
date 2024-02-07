dc4e920b82a6a558afcb5303fbf4f70d284ef45b
EnigmaWU <enigma0702@gmail.com>
2024-02-07 10:30:57 +0800

    Reorder include statements in TOS_Base4ALL.h

--- ThinkOS/TOS_Base4ALL.h
+++ ThinkOS/TOS_Base4ALL.h
@@ -1,10 +1,11 @@
 #ifndef __TOS_BASE4ALL_H__
 #define __TOS_BASE4ALL_H__
 
-#include "TOS_BaseObjectTypes.h"
 #include "TOS_BaseCommand.h"
-#include "TOS_BaseOption.h"
-#include "TOS_BaseObject.h"
 #include "TOS_BaseHelpers.h"
+#include "TOS_BaseObject.h"
+#include "TOS_BaseObjectTypes.h"
+#include "TOS_BaseOption.h"
+#include "TOS_BaseTypes.h"
 
 #endif
